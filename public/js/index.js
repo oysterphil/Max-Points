2dde1d7b4d40c6b314fe46f6214ee92e9792089d
oysterphil <po7831a@student.american.edu>
2017-02-03 02:13:25 -0500

    Getting there. I still need to fix the card details section as well as the PP and TOS links. Overall, it is pretty good.

--- public/js/index.js
+++ public/js/index.js
@@ -1540,16 +1540,85 @@ var model = {
                     amazon: 5,
                     wholesaleClubs: 5
                 },
-                howCardIncreasesPoints: [
-                    'No annual fee.',
-                    '1% cash back everywhere and 2% in year one.',
-                    '5% cash back on rotating categories (up to $1,500 spent per quarter) and 10% in year 1.',
-                    'Typical categories: gas & local transportation including Uber and Lyft in Q1, restaurants, movies & wholesale clubs in Q2, Amazon & home improvement stores in Q3 then department stores & Amazon in Q4.',
-                    'No foreign transaction fees.',
-                    'Free TransUnion FICO score updated monthly.',
-                    'Access to Discover Deals online shopping portal.',
-                    'All cash back doubled at the end of year one.'
-                ]
+                cardDetails: {
+                    cardImage: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                    bonusAndEarningsDetails: [
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        }
+                    ],
+                    annualFeeDetails: [
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        }
+                    ],
+                    extraBenefitsDetails: [
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        }
+                    ],
+                    redemptionDetails: [
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        }
+                    ],
+                    analysisDetails: [
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        }
+                    ],
+                    signupBonus: {
+                        bonusValue: 50,
+                        bonusCopy: 'Here is some signup bonus copy.'
+                    },
+                    earningRateIncrease: 25,
+                    affiliateLink: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png'
+                }
             },
             {
                 cardName: 'Discover It Secured Card',
@@ -1559,15 +1628,85 @@ var model = {
                     gas: 2,
                     diningOut: 2
                 },
-                howCardIncreasesPoints: [
-                    'The highest points-earning secured card without an annual fee.',
-                    'Your initial deposit is your credit line and your card is reviewed automatically after the first year in order to refund the deposit and transition the card to a regular unsecured card.',
-                    '1% cash back (2% in year one) everywhere.',
-                    '2% cash back (4% in year one) on gas and restaurants (up to $1,000 in combined purchases quarterly).',
-                    'No foreign transaction fees.',
-                    'Free TransUnion FICO score updated monthly.',
-                    'All cash back doubled at the end of year one.'
-                ]
+                cardDetails: {
+                    cardImage: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                    bonusAndEarningsDetails: [
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        }
+                    ],
+                    annualFeeDetails: [
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        }
+                    ],
+                    extraBenefitsDetails: [
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        }
+                    ],
+                    redemptionDetails: [
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        }
+                    ],
+                    analysisDetails: [
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        }
+                    ],
+                    signupBonus: {
+                        bonusValue: 500,
+                        bonusCopy: 'Here is some signup bonus copy.'
+                    },
+                    earningRateIncrease: 100,
+                    affiliateLink: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png'
+                }
             },
             {
                 cardName: 'Capital One Quicksilver One MasterCard or Visa',
@@ -1606,14 +1745,85 @@ var model = {
                     drugstores: 5,
                     departmentStores: 5
                 },
-                signUpBonus: '15,000 points after spending $500 in the first three months.',
-                howCardIncreasesPoints: [
-                    '1% cash back everywhere.',
-                    '5% cash back on rotating categories (up to $1,500 spent per quarter).',
-                    'Typical categories: gas & commuter passes in Q1,grocery stores and drugstores in Q2, restaurants in Q3 then department stores & drugstores in Q4',
-                    'Access to Chase’s online shopping portal.',
-                    'Points are cash back but if the same cardholder has the Chase Sapphire Preferred, Reserve, Ink Plus, Ink Bold or Ink Preferred, then all points are transferable to many frequent flyer and hotel loyalty programs for even more value.'
-                ]
+                cardDetails: {
+                    cardImage: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                    bonusAndEarningsDetails: [
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        }
+                    ],
+                    annualFeeDetails: [
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        }
+                    ],
+                    extraBenefitsDetails: [
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        }
+                    ],
+                    redemptionDetails: [
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        }
+                    ],
+                    analysisDetails: [
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        },
+                        {
+                            icon: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png',
+                            detail: 'Here are some details.'
+                        }
+                    ],
+                    signupBonus: {
+                        bonusValue: 150,
+                        bonusCopy: 'Here is some signup bonus copy.'
+                    },
+                    earningRateIncrease: 50,
+                    affiliateLink: 'https://cache.addthiscdn.com/icons/v3/thumbs/32x32/email.png'
+                }
             },
             {
                 cardName: 'Barclaycard Arrival Plus World Elite MasterCard',
