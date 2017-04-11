731cd7d35508cee0be35b64846f09320b1962868
oysterphil <po7831a@student.american.edu>
2017-04-11 03:01:15 -0400

    Fixed another bug where, after logging in, it was not picking up if you deleted a card from the calculator on the second pass and persisting it across future sessions.

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,7 @@
 var model = { 
     appState: {
         loginLoading: false,
+        mobileView: null,
         uid: null,
         landingPage: true,
         calculator: false,
@@ -6178,6 +6179,14 @@ var model = {
             // Insert LivIcons
             model.controllers.insertLivIconsForCalculator();
 
+            // Mobile or Not?
+            window.mobileAndTabletcheck = function() {
+              var check = false;
+              (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+              model.appState.mobileView = check;
+            };
+            window.mobileAndTabletcheck();
+
             // Show Points Calculator Template
             document.getElementById('pointsCalculator').style.display = "inline";
 
@@ -6936,6 +6945,16 @@ var model = {
 
             var checkCards = ['card1','card2','card3','card4','card5','card6','card7','card8','card9',]
             
+            if (!model.appState.mobileView) {
+                for (var i = 0; i < checkCards.length; i ++) {
+                    document.getElementById(checkCards[i] + 'Mobile').value = '';
+                }
+            } else {
+                for (var i = 0; i < checkCards.length; i ++) {
+                    document.getElementById(checkCards[i]).value = '';
+                }
+            }
+
             for (var i = 0; i < checkCards.length; i++) {
                 if (document.getElementById(checkCards[i]).value) {
                     compiledCardValues[i] = document.getElementById(checkCards[i]).value;
@@ -7480,92 +7499,103 @@ var model = {
             // Establish Baseline Variables
 
             var yearlySpend = model.cards.currentStatusBasedOnSelections.monthlySpendValue * 12;
-
-            var earningRateCats = [
-                'everywhere',
-                'gas',
-                'groceries',
-                'travel',
-                'diningOut'
-            ];
-
-            // Calculate Current Earning Rate 
-
-            var currentEarningRateRatios = {
-                everywhere: 0.2,
-                gas: 0.2,
-                groceries: 0.2,
-                travel: 0.2,
-                diningOut: 0.2
-            }
-
             var currentEarningRate = 0;
+            var newEarningRate = 0;
 
-            earningRateCats.forEach(function(cat) {
-                if (!model.cards.currentStatusBasedOnSelections[cat]) {
-                    delete currentEarningRateRatios[cat];
-                    currentEarningRateRatios.everywhere += 0.2;
+            if (model.cards.userSelections.length > 0) {
+
+                var earningRateCats = [
+                    'everywhere',
+                    'gas',
+                    'groceries',
+                    'travel',
+                    'diningOut'
+                ];
+
+                // Calculate Current Earning Rate 
+
+                var currentEarningRateRatios = {
+                    everywhere: 0.2,
+                    gas: 0.2,
+                    groceries: 0.2,
+                    travel: 0.2,
+                    diningOut: 0.2
                 }
-            });
 
-            if (model.cards.userSelections.length === 0) {
-                currentEarningRate = 0;
-            } else {   
+                earningRateCats.forEach(function(cat) {
+                    if (!model.cards.currentStatusBasedOnSelections[cat]) {
+                        delete currentEarningRateRatios[cat];
+                        currentEarningRateRatios.everywhere += 0.2;
+                    }
+                });
+
                 for (var cat in currentEarningRateRatios) {
                     currentEarningRate += (currentEarningRateRatios[cat] * yearlySpend * model.cards.currentStatusBasedOnSelections[cat]);
                 }
-            }
 
-            console.log('currentEarningRate');
-            console.log(currentEarningRate);
+                console.log('currentEarningRate');
+                console.log(currentEarningRate);
 
-            // Calculate Future Earning Rate
+                // Calculate Future Earning Rate
 
-            var newEarningRateRatios = {
-                everywhere: 0.2,
-                gas: 0.2,
-                groceries: 0.2,
-                travel: 0.2,
-                diningOut: 0.2
-            }
-
-            var newEarningRate = 0;
-
-            // Add New Card to Current Status Based on Selections
-            
-            var newCurrentStatus={}; 
-
-            earningRateCats.forEach(function(cat) {
-                if (model.cards.currentStatusBasedOnSelections[cat]) {
-                    newCurrentStatus[cat] = model.cards.currentStatusBasedOnSelections[cat];
+                var newEarningRateRatios = {
+                    everywhere: 0.2,
+                    gas: 0.2,
+                    groceries: 0.2,
+                    travel: 0.2,
+                    diningOut: 0.2
                 }
-            });
 
-            console.log(model.cards.currentStatusBasedOnSelections);
-            console.log(newCurrentStatus);
+                // Add New Card to Current Status Based on Selections
+                
+                var newCurrentStatus={}; 
 
-            earningRateCats.forEach(function(cat) {
-                if (model.cards.currentDisplayRec.categories[cat] && newCurrentStatus[cat]) {                
-                    if (model.cards.currentDisplayRec.categories[cat] > model.cards.currentStatusBasedOnSelections[cat]) {
-                        newCurrentStatus[cat] = model.cards.currentDisplayRec.categories[cat];
+                earningRateCats.forEach(function(cat) {
+                    if (model.cards.currentStatusBasedOnSelections[cat]) {
+                        newCurrentStatus[cat] = model.cards.currentStatusBasedOnSelections[cat];
                     }
+                });
+
+                console.log(model.cards.currentStatusBasedOnSelections);
+                console.log(newCurrentStatus);
+
+                earningRateCats.forEach(function(cat) {
+                    if (model.cards.currentDisplayRec.categories[cat] && newCurrentStatus[cat]) {                
+                        if (model.cards.currentDisplayRec.categories[cat] > model.cards.currentStatusBasedOnSelections[cat]) {
+                            newCurrentStatus[cat] = model.cards.currentDisplayRec.categories[cat];
+                        }
+                    }
+                });
+
+
+                earningRateCats.forEach(function(cat) {
+                    if (!newCurrentStatus[cat]) {
+                        delete newEarningRateRatios[cat];
+                        newEarningRateRatios.everywhere += 0.2;
+                    }
+                });
+
+                for (var cat in newEarningRateRatios) {
+                    newEarningRate += (newEarningRateRatios[cat] * yearlySpend * newCurrentStatus[cat]);
                 }
-            });
 
-
-            earningRateCats.forEach(function(cat) {
-                if (!newCurrentStatus[cat]) {
-                    delete newEarningRateRatios[cat];
-                    newEarningRateRatios.everywhere += 0.2;
+                console.log('newEarningRate');
+                console.log(newEarningRate);
+            } else {
+                var newEarningRateRatios = {
+                    everywhere: 0.2,
+                    gas: 0.2,
+                    groceries: 0.2,
+                    travel: 0.2,
+                    diningOut: 0.2
                 }
-            });
 
-            for (var cat in newEarningRateRatios) {
-                newEarningRate += (newEarningRateRatios[cat] * yearlySpend * newCurrentStatus[cat]);
+                for (var cat in newEarningRateRatios) {
+                    newEarningRate += (newEarningRateRatios[cat] * yearlySpend);
+                }
             }
 
-            console.log('newEarningRate');
-            console.log(newEarningRate);
+
 
             // Add Data to Baseline Earning Rate
             var baselineEarningRate = {
@@ -7778,6 +7808,8 @@ var model = {
             for (var i = 1; i < 10; i++) {
                 if (model.cards.userSelections[(i-1)]) {
                     calcEntry['card' + i] = model.cards.userSelections[(i-1)].cardName;
+                } else {
+                    calcEntry['card' + i] = null;
                 }
             }
 
